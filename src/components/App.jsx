642c7b96e2bf86cc68974f2e78c5d06df14e31d3
ketan7705 <hi@example.com>
2020-04-05 20:55:47 +0530

    Changes In the Note Componets & and added the Few Notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
-function App(){
-    return (
+function App() {
+  return (
     <div>
-        <Header />
-        <Note className="note" />
-        <Footer />
+      <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
     </div>
-    )
-};
+  );
+}
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Note(){
-    return <div className="note">
-        <h1>This is the Title</h1>
-        <p>This is the Content</p>
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
+  );
 }
 
 export default Note;
+
